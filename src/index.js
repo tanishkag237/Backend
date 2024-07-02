65703d272713243dcaa010bd186d7a3e376a2caa
tanishka237 <tg000237@gmail.com>
2024-07-02 23:35:30 +0530

    final commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import dotenv from "dotenv"
 
+
 import DBconnect from "./db/index.js"
 
 dotenv.config({
     path:'./env'
 })
-DBconnect();
+DBconnect()
+.then(()=>{
+    app.listen((process.env.PORT||8000,()=>{
+        console.log(`server is running on port : ${process.env.PORT}`);
+    }))
+
+})
+.catch((err)=>{
+    console.log("DB connection failed !! ",err);
+})
